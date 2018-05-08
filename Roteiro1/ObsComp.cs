cd845ea02a2f5f3482918ee3e39ab52bba4f35fc
phillima <phyllipe_slf@yahoo.com.br>
2018-05-07 21:47:55 -0300

    Atualizando o Roteiro 1
    Atualizando o Roteiro 1

---
+++ Roteiro1/ObsComp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Classe para definir o comportamento do obstaculo
+/// </summary>
+public class ObsComp : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Tempo para reiniciar o jogo")]
+    private float tempoDestruir = 2.0f;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.GetComponent<JogadorComp>()) {
+            Invoke("Reset", tempoDestruir);
+        }
+    }
+
+    private void Reset() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
