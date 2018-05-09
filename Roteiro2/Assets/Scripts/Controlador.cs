c5c5a93647ebd5c2e2d934e785b147e7e09dd61c
phillima <phyllipe_slf@yahoo.com.br>
2018-05-09 12:38:14 -0300

    Roteiro 2
    Roteiro 2

---
+++ Roteiro2/Assets/Scripts/Controlador.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Referencia para o tile basico")]
+    private Transform tileBasico;
+
+    [SerializeField]
+    [Tooltip("Referencia para o obstaculo")]
+    private Transform obstaculo;
+
+    /// <summary>
+    /// Numero de tiles iniciais
+    /// </summary>
+    [SerializeField]
+    [Range(5,20)]
+    private int numTilesSpawn;
+
+    [SerializeField]
+    [Tooltip("Numero de tiles sem obstaculo")]
+    [Range(1,5)]
+    private int numTileSemOBS;
+
+    /// <summary>
+    /// Posicao do primeiro tile
+    /// </summary>
+    private Vector3 posInicial = new Vector3(0,0,-5);
+
+    /// <summary>
+    /// Posicao do proximo tile
+    /// </summary>
+    private Vector3 proxTilePos;
+
+    /// <summary>
+    /// Rotacao do proximo tile
+    /// </summary>
+    private Quaternion proxTileRot;
+
+	// Use this for initialization
+	void Start () {
+
+        //Define a posicao e rotacao do primeiro tile
+        proxTilePos = posInicial;
+        proxTileRot = Quaternion.identity;
+
+        
+        for (int i = 0; i < numTilesSpawn; i++) {
+            SpawnTiles(i >= numTileSemOBS);
+        }
+
+	}
+
+    
+    public void SpawnTiles(bool temObs = true) {
+        //Cria novo tile
+        var novoTile = Instantiate(tileBasico, proxTilePos, proxTileRot);
+
+        //Prepara a posicao e rotacao do proximo tile
+        var proxTile = novoTile.Find("PontoSpawn");
+        proxTilePos = proxTile.position;
+        proxTileRot = proxTile.rotation;
+
+        //Verifica se devemos criar obstaculos
+        if (!temObs)
+            return;
+
+        //Tratar a criacao de obstaculos
+        var pontosObs = new List<GameObject>();
+
+        foreach(Transform filho in novoTile) {
+            if (filho.CompareTag("Obstaculo")) {
+                pontosObs.Add(filho.gameObject);
+            }
+        }
+
+        //Garantir que existe pelo menos um ponto disponivel para o obstaculo
+        if(pontosObs.Count > 0) {
+
+            //Escolhemos um ponto dos tres possiveis.
+            var pontoObs = pontosObs[Random.Range(0, pontosObs.Count)];
+
+            //Obtemos a posicao deste ponto
+            var pontoPos = pontoObs.transform.position;
+
+            //Criamos o novo obstaculo e fazemos o ponto de spaw ser o game object pai
+            var novoObs = Instantiate(obstaculo, pontoPos, Quaternion.identity, pontoObs.transform);
+
+        }
+    }
+}
---
+++ Roteiro2/Assets/Scripts/JogadorComp.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Classe que define o comportamento do jogador
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class JogadorComp : MonoBehaviour {
+
+    public enum TipoMovimentoHorizontal {
+        Acelerometro,
+        Touch
+    }
+
+    [SerializeField]
+    [Tooltip("Define o tipo de movimento horizontal")]
+    private TipoMovimentoHorizontal TipoMovimento = TipoMovimentoHorizontal.Touch;
+
+    /// <summary>
+    /// Referencia para o corpo rigido da bola
+    /// </summary>
+    private Rigidbody rb;
+
+    [SerializeField]
+    [Tooltip("Controla a velocidade de rolamento")]
+    [Range(1, 20)]
+    private float velocidadeRolamento = 5.0f;
+
+    [SerializeField]
+    [Tooltip("Controla a velocidade de esquiva")]
+    [Range(1, 20)]
+    private float velocidadeEsquiva = 5.0f;
+
+
+    [Header("Atributos responsaveis pelo swipe")]
+    [SerializeField]
+    [Tooltip("Determina a distancia minima que o dedo do jogador deve percorrer para ser considerado um swipe")]
+    private float minDisSwipe = 2.0f;
+
+    [SerializeField]
+    [Tooltip("Distacia percorrida pela bola depois do swipe")]
+    private float swipeMove = 2.0f;
+
+    /// <summary>
+    /// Ponto inicial do touch
+    /// </summary>
+    private Vector2 pontoInicial;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        //Detectando uso das teclas A e D
+        float velocidadeHorizontal = Input.GetAxis("Horizontal") * velocidadeEsquiva;
+
+#if UNITY_STANDALONE
+        //Verifica se houve clique com o mouse na tela atraves do botao direito
+        if (Input.GetMouseButton(0)) {
+            velocidadeHorizontal = CalculaMovimento(Input.mousePosition);
+            TocarObjeto(Input.mousePosition);
+        }
+#elif UNITY_ANDROID || UNITY_IOS
+        if(TipoMovimento == TipoMovimentoHorizontal.Acelerometro) {
+            velocidadeHorizontal = Input.acceleration.x * velocidadeRolamento;
+        } else {
+            if (Input.touchCount > 0) {
+                //Desejamos apenas o primeiro touch na tela
+                Touch touch = Input.touches[0];
+                velocidadeHorizontal = CalculaMovimento(touch.position);
+
+                SwipeTeleporte(touch);
+
+                TocarObjeto(touch.position);
+            }
+        }
+#endif
+       
+
+        rb.AddForce(velocidadeHorizontal, 0, velocidadeRolamento);
+    }
+
+    private float CalculaMovimento(Vector2 screenSpacePos) {
+        var posPortView = Camera.main.ScreenToViewportPoint(screenSpacePos);
+        float direcaoX;
+        //Toque no lado direito
+        if (posPortView.x > 0.5) {
+            direcaoX = 1;
+        } else {//Toque no lado esquerdao
+            direcaoX = -1;
+        }
+        return direcaoX * velocidadeEsquiva;
+
+    }
+
+    /// <summary>
+    /// Funcao que trata o swipe
+    /// </summary>
+    private void SwipeTeleporte(Touch toque) {
+
+        if (toque.phase == TouchPhase.Began) {
+            pontoInicial = toque.position;
+        }
+        //Verifica se o swipe chegou ao fim (se o jogador soltou o dedo)
+        else if (toque.phase == TouchPhase.Ended) {
+
+            Vector2 pontoFinal = toque.position;
+            Vector3 direcaoMove;
+            //Calcula a diferenca
+            float diferencaX = pontoFinal.x - pontoInicial.x;
+            if (Mathf.Abs(diferencaX) >= minDisSwipe) {//A distancia percorrida e o suficiente para ser considerada um swipe
+                //Verifica a direcao do swipe
+                if (diferencaX < 0)
+                    direcaoMove = Vector3.left;
+                else
+                    direcaoMove = Vector3.right;
+
+            } else //Distancia insuficiente para ser considerado um swipe
+                return;
+
+            //Mas antes de executarmos o swipe, precisamos verifica se a bola nao ira colidir com algum obstaculo
+            //Fazemos isso usando o Raycast
+            RaycastHit hit;
+
+            if (!rb.SweepTest(direcaoMove, out hit, swipeMove)) {
+                rb.MovePosition(rb.position + (direcaoMove * swipeMove));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Metodo para identificar se objetos foram tocados 
+    /// </summary>
+    /// <param name="pos">A poscicao tocada/clicada nada tela</param>
+    private static void TocarObjeto(Vector3 pos) {
+        Ray posicaoTelaRay = Camera.main.ScreenPointToRay(pos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(posicaoTelaRay, out hit)) {
+            hit.transform.SendMessage("ObjetoTocado", SendMessageOptions.DontRequireReceiver);
+            print(hit.transform.name);
+        }
+    }
+}
+
