2d0e5fb95d912c118ab2333a1445e59441f61b7c
phillima <phyllipe_slf@yahoo.com.br>
2018-05-09 22:52:16 -0300

    Roteiro 1
    Roteiro 1

---
+++ Roteiro1/Assets/Scripts/FimTileComp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FimTileComp : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Tempo para destruir o tile anterior")]
+    private float tempoDestruir = 2.0f;
+
+    private void OnTriggerEnter(Collider other) {
+
+        //Verifica se colidiu com a bola
+        if (other.GetComponent<JogadorComp>()) {
+
+            //Cria um novo tile
+            GameObject.FindObjectOfType<Controlador>().SpawnTiles();
+            //Destroi o tile atual
+            Destroy(transform.parent.gameObject, tempoDestruir);
+
+        }
+    }
+}
---
+++ Roteiro1/Assets/Scripts/JogadorComp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Classe que define o comportamento do jogador
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class JogadorComp : MonoBehaviour {
+
+    /// <summary>
+    /// Referencia para o corpo rigido da bola
+    /// </summary>
+    private Rigidbody rb;
+
+    [SerializeField]
+    [Tooltip("Controla a velocidade de rolamento")]
+    [Range(1,20)]
+    private float velocidadeRolamento = 5.0f;
+
+    [SerializeField]
+    [Tooltip("Controla a velocidade de esquiva")]
+    [Range(1, 20)]
+    private float velocidadeEsquiva = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        //Invoke("FreezeY", 2.0f);
+	}
+
+    void Update() {
+        float velocidadeHorizontal = Input.GetAxis("Horizontal") * velocidadeEsquiva;
+        rb.AddForce(velocidadeHorizontal, 0, velocidadeRolamento);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        
+	}
+
+    private void FreezeY() {
+        rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY;
+    }
+
+    
+}
