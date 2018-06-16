ab423c64a6bfd01ba3fa2a9ef0ae7952bf97f89b
phillima <phyllipe_slf@yahoo.com.br>
2018-06-16 10:13:29 -0300

    1 - Parte
    1 - Parte

---
+++ Roteiro5/Assets/Scripts/GameOver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    [SerializeField]
+    private LevelControle levelControle;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        levelControle.CarregaLevel("TelaVitoria");
+    }
+
+}
---
+++ Roteiro5/Assets/Scripts/LevelControle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControle : MonoBehaviour {
+
+	public void CarregaLevel(string sceneNome) {
+        SceneManager.LoadScene(sceneNome);
+    }
+
+}
