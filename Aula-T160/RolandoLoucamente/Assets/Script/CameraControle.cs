c0eb931a42c41f12a91a92b8e0a236b18ab8b44f
phillima <phyllipe_slf@yahoo.com.br>
2018-06-30 13:40:31 -0300

    Parte 2
    Parte 2

---
+++ Aula-T160/RolandoLoucamente/Assets/Script/CameraControle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControle : MonoBehaviour {
+
+    JogadorControle jogador;
+ 
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        jogador = FindObjectOfType<JogadorControle>();
+        offset = jogador.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Aula-T160/RolandoLoucamente/Assets/Script/JogadorControle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class JogadorControle : MonoBehaviour {
+
+    /// <summary>
+    /// Referencia para a variavel rigidbody
+    /// </summary>
+    Rigidbody rb;
+
+    [SerializeField]
+    [Tooltip("A velocidade de deslocamento horizontal")]
+    [Range(1, 20)]
+    float velocidadeEsquiva;
+
+    [Tooltip("A velocidade de deslocamento frontal")]
+    [Range(1, 20)]
+    [SerializeField]
+    float velocidadeRolamento = 5.0f;
+
+    [SerializeField]
+    Vector3 posicaoInicial;
+   
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+       /* posicaoInicial = transform.position;
+        posicaoInicial.x = 2.0f;
+        transform.position = posicaoInicial;*/
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var direcaoHorizontal =
+            Input.GetAxis("Horizontal");
+
+        rb.AddForce(velocidadeEsquiva*direcaoHorizontal,
+                    0,
+                    velocidadeRolamento);
+	}
+}
