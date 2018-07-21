c79954f3553a579a12512c5f932ccb4e5532af79
phillima <phyllipe_slf@yahoo.com.br>
2018-07-21 13:30:28 -0300

    Aula 5 - T160 - Parte2
    Aula 5 - T160 - Parte2

---
+++ Roteiro5 - QuebraQuebra/Quebra-T160/Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    //Referencia para o rigidbody da bola
+    Rigidbody2D ballRB;
+    //Referncia para a plataforma (paddle)
+    Paddle paddle;
+
+    //Posicao inicial da bola
+    Vector3 offset = new Vector3(0f,0.6f,0f);
+
+	// Use this for initialization
+	void Start () {
+        ballRB = GetComponent<Rigidbody2D>();
+        ballRB.isKinematic = true;
+        paddle = FindObjectOfType<Paddle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!LevelControl.hasGameStarted) {
+            if (Input.GetMouseButtonDown(0)) {
+                LaunchBall();
+            } else {
+                MoveWithPaddle();
+            }
+        }
+	}
+    //Metodo para mover a bola agarrado com a plataforma
+    void MoveWithPaddle() {
+        transform.position = paddle.transform.position
+                                + offset;
+    }
+
+    //Metodo para lancar a bola
+    void LaunchBall() {
+        ballRB.isKinematic = false;
+        ballRB.AddForce(Vector2.up * 15,ForceMode2D.Impulse);
+        LevelControl.hasGameStarted = true;
+    }
+}
---
+++ Roteiro5 - QuebraQuebra/Quebra-T160/Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    
+    //Referencia para a camera principal
+    Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ManualMovement();
+	}
+
+    //Metodo para mover a plataforma (paddle)
+    void ManualMovement() {
+        Vector3 paddlePos = new Vector3(0,1,0);
+        paddlePos.x =
+            Mathf.Clamp(mainCamera.
+            ScreenToWorldPoint(Input.mousePosition).x,
+            1.2f,
+            14.8f);
+        transform.position = paddlePos;
+    }
+}
