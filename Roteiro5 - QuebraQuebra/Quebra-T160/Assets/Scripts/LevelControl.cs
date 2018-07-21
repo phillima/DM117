64f112a649c979d57931c14fa591fb033f1484b1
phillima <phyllipe_slf@yahoo.com.br>
2018-07-21 10:07:21 -0300

    Aula 5 - T160 Parte 1
    Aula 5 - T160 Parte 1

---
+++ Roteiro5 - QuebraQuebra/Quebra-T160/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour {
+
+    //Control number of Balls in the game
+    public static int numBalls;
+
+    //Control if there are no more block
+    //on the scene
+    public static int numBlocks;
+
+    //Control if the game has started
+    public static bool hasGameStarted = false;
+
+    void Awake() {
+        hasGameStarted = false;
+        numBalls = 0;
+        numBlocks = 0;
+    }
+
+    public void LoadlLevel(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene()
+            .buildIndex + 1);
+    }
+
+    public void BlockDestroyed() {
+        numBlocks--;
+        if(numBlocks <= 0) {
+            LoadNextLevel();
+        }
+    }
+
+    public void BallFallen() {
+        LoadlLevel("TelaGameOver");
+    }
+
+}
---
+++ Roteiro5 - QuebraQuebra/Quebra-T160/Assets/Scripts/MusicControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour {
+
+    public static MusicControl 
+        musicaControle = null;
+
+    private void Awake() {
+        if(musicaControle != null) {
+            Destroy(gameObject);
+        } else {
+            musicaControle = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
